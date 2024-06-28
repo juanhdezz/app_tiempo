900bb3fb0352a4f80b6544e22acd51f6f48e6372
juan <jhernandezsanchezagesta@gmail.com>
2024-06-28 11:29:32 +0200

    probando mis cosas

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react"
+
+console.log("Hola mundo")
+const Navbar = () => {
+    return(
+    <nav className="navbar text-light mb-5">
+        <div className="container-fluid">
+            <h2 className="mx-auto"> Predicción Meteorológica </h2>
+        </div>
+    </nav>
+    )
+
+}
+
+export default Navbar ;
